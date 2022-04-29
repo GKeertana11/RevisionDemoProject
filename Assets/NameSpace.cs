54b4c9d79380f1ded67146ef6ac5adad0307ab12
KeertanaG <keertana.govte@yesgnome.com>
2022-04-29 15:00:04 +0530

    System.Serializable Done.

--- Assets/NameSpace.cs
+++ Assets/NameSpace.cs
@@ -9,11 +9,18 @@ using MyNewNameSpace.MyUI;
 
 public class NameSpace : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+   public  MyNameSpaceClass MyDemoClass = new MyNameSpaceClass();
+
+// Start is called before the first frame update
+void Start()
     {
         MyNameSpaceClass.MyPrint();
         MyUIClass.UIprint();
+        Debug.Log(MyDemoClass.playerRotation);
+        Debug.Log(MyDemoClass.playerSpeed);
+        Debug.Log(MyDemoClass.isAlive);
+        Debug.Log(MyDemoClass.name);
+
     }
     // Update is called once per frame
     void Update()
--- Assets/NamespaceDemo.cs
+++ Assets/NamespaceDemo.cs
@@ -4,8 +4,14 @@ using UnityEngine;
 
 namespace MyNewNameSpace
 {
+    [System.Serializable]
     public class MyNameSpaceClass 
     {
+
+        public int playerSpeed;
+        public float playerRotation;
+        public bool isAlive;
+        public string name;
         public static void MyPrint()
         {
             Debug.Log("My print method in namespace");
